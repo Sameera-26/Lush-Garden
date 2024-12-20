ba187185e753ab0a090ef539dea5a880e4171d54
Hamza zafar <alamsameera26@gmail.com>
2024-12-20 16:27:58 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
+</script>
+
+<template>
+  <HomeView></HomeView>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-[url('@/assets/hero.png')] h-screen w-screen bg-cover bg-center bg-no-repeat">
+    <Main> </Main>
+    <div class="flex flex-col items-center justify-center my-11 py-12">
+      <h1 class="text-white text-6xl font-semibold">Nature's Beauty Delivered to You</h1>
+      <div class="py-12">
+        <h3 class="text-white text-center text-lg m font-normal text-pretty">
+          Nature's beauty is just a click away with our online flower and plant shop. We offer wide
+        </h3>
+        <h3 class="text-white text-center text-lg m font-normal text-pretty">
+          a wide variety of flowers that will bring a touch of nature to your home!
+        </h3>
+      </div>
+      <div class="flex flex-row">
+        <button
+          class="border-2 rounded-sm text-white px-12 py-2 bg-[#285A43] hover:bg-white/20 hover:text-[#285A43] hover:font-semibold border-[#285A43]"
+        >
+          Book Now
+        </button>
+        <button
+          class="border-2 rounded-sm flex text-white mx-7 px-12 py-2 hover:bg-[#285A43] hover:border-[#285A43]"
+        >
+          <img class="mx-2" src="@/assets/play.png" />
+          Watch Video
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+export default {
+  components: {},
+}
+</script>
